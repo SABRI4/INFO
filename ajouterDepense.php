0b87618827a42c46b9660e618aa8d1e2cff7b2d0
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-05-15 00:13:57 +0200

    Update ajouterDepense.php

--- ajouterDepense.php
+++ ajouterDepense.php
@@ -1,22 +1,38 @@
 <?php
+session_start();
 include 'connect.php';
 
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['user_id'])) {
+    echo'<script type="text/javascript"> alert("Vous devez être connecté pour effectuer cette action."); window.location = "Ajout.php"; </script>';
+    exit;  
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user_id = $_SESSION['user_id'];
     $categorie = $conn->real_escape_string($_POST['categorie']);
     $montant = $conn->real_escape_string($_POST['montant']);
     $date = $conn->real_escape_string($_POST['date']);
     $description = $conn->real_escape_string($_POST['description']);
 
-    $sql = "INSERT INTO depenses (categorie, montant, date, description)
-    VALUES ('$categorie', '$montant', '$date', '$description')";
+    //Requete SQL
+    $sql = "INSERT INTO depenses (user_id, categorie, montant, date, description) VALUES (?, ?, ?, ?, ?)";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Dépense ajoutée avec succès.";
-        // Rediriger vers une autre page ou afficher un message de succès
-    } else {
-        echo "Erreur lors de l'ajout de la dépense : " . $conn->error;
+    $stmt = $conn->prepare($sql);
+    if ($stmt === false) {
+        echo "Erreur lors de la préparation de la requête : " . $conn->error;
+        exit;
     }
 
+    // Lier les paramètres et exécuter la requête
+    $stmt->bind_param("isdss", $user_id, $categorie, $montant, $date, $description);
+    if ($stmt->execute()) {
+        echo "Dépense ajoutée avec succès.";
+        
+    } else {
+        echo "Erreur lors de l'ajout de la dépense : " . $stmt->error;
+    }
+    $stmt->close();
     $conn->close();
 }
 
