3d6f7e29470d762a746db7bb0e1096009b3c8240
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-05-15 01:00:51 +0200

    Update Historique.php

--- Historique.php
+++ Historique.php
@@ -8,6 +8,21 @@
   <script src="historique.js" defer></script>
 </head>
 <body>
+<div>
+<?php
+    session_start();
+    // Vérifier si l'utilisateur est connecté
+    if (isset($_SESSION['user_id'])) {
+        // L'utilisateur est connecté, afficher le bouton de déconnexion
+        echo '<form action="logout.php" method="post">
+                  <button type="submit">Déconnexion</button>
+              </form>';
+    } else {
+        // L'utilisateur n'est pas connecté, afficher un lien vers la page de connexion
+        echo '<a href="connexion.html">Connexion</a>';
+    }
+    ?>
+</div>
 <h2>Historique de vos dépenses :</h2>
 <label for="tri">Choisissez un ordre de tri :</label>
 <select id="tri" onchange="trierDepenses()">
