b0dba76f5cafc0614a648b0e10f9806aed651a0a
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-05-18 20:10:20 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $conn->real_escape_string($_POST['username']);
     $email = $conn->real_escape_string($_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
+    $budget = $conn->real_escape_string($_POST['budget']);
     // Récupérer les informations sur la photo téléchargée
     $photo_name = $_FILES["photo"]["name"];
     $photo_tmp_name = $_FILES["photo"]["tmp_name"];
@@ -15,11 +15,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Déplacer la photo téléchargée vers le dossier de destination
     if (move_uploaded_file($photo_tmp_name, $photo_destination)) {
         // Préparer et exécuter la requête d'insertion
-        $sql = "INSERT INTO users (username, email, password, photo) VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO users (username, email, password, photo, budget) VALUES (?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($sql);
 
         if ($stmt) {
-            $stmt->bind_param("ssss", $username, $email, $password, $photo_destination);
+            $stmt->bind_param("ssssi", $username, $email, $password, $photo_destination, $budget);
             if ($stmt->execute()) {
                 echo "Compte créé avec succès.";
                 header("Location: connexion.html"); // Rediriger vers le formulaire de connexion
