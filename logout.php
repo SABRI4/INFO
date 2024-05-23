1c1b79c83e02c94a8a6fbaeadcb138a529b1c533
Flavoux <153535698+Flavoux@users.noreply.github.com>
2024-05-23 15:46:18 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -17,6 +17,6 @@ if (ini_get("session.use_cookies")) {
 session_destroy();
 
 // Rediriger l'utilisateur vers la page de connexion
-header("Location: connexion.html");
+header("Location: Accueil.php");
 exit();
 ?>
