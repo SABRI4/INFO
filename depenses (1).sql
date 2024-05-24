cdf00c8428d88061e64a918e63173884f3bc5d7e
Flavoux <153535698+Flavoux@users.noreply.github.com>
2024-05-24 19:59:03 +0200

    Update depenses (1).sql

--- depenses (1).sql
+++ depenses (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 23 mai 2024 à 13:16
+-- Généré le : ven. 24 mai 2024 à 17:57
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `depenses` (
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `depenses`
@@ -63,7 +63,45 @@ INSERT INTO `depenses` (`id`, `user_id`, `categorie`, `montant`, `date`, `descri
 (35, 5, 'alimentation', 2129.00, '2024-05-29', ''),
 (37, 5, 'alimentation', 30.00, '2024-05-16', ''),
 (48, 8, 'alimentation', 125.00, '2024-03-23', ''),
-(49, 8, 'alimentation', 256.00, '2024-05-25', '');
+(49, 8, 'alimentation', 256.00, '2024-05-25', ''),
+(50, 11, 'loisirs', 34.00, '2024-05-24', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message_replies`
+--
+
+DROP TABLE IF EXISTS `message_replies`;
+CREATE TABLE IF NOT EXISTS `message_replies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `posted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `message_id` (`message_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `public_messages`
+--
+
+DROP TABLE IF EXISTS `public_messages`;
+CREATE TABLE IF NOT EXISTS `public_messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `posted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -81,18 +119,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `ACTIVE` tinyint(1) DEFAULT NULL,
   `VIP` tinyint(1) DEFAULT NULL,
   `budget` int NOT NULL,
-  `role` enum('user','admin') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user',
-  `couleur` varchar(8) COLLATE utf8mb4_general_ci DEFAULT '#0c1b8c',
+  `role` enum('user','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user',
+  `couleur` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '#0c1b8c',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `photo`, `ACTIVE`, `VIP`, `budget`, `role`, `couleur`) VALUES
+(5, 'Younes', '$2y$10$bNeUZxKhzrFwjhp.oxKkA.TInzixEaF4i/4Bw3ybh/wpY.VZyCHBy', 'younesysabri@hotmail.fr', 'uploads/images.jpg', 0, 0, 150, 'user', '#0c1b8c'),
 (6, 'admin', '$2y$10$bNeUZxKhzrFwjhp.oxKkA.TInzixEaF4i/4Bw3ybh/wpY.VZyCHBy', 'admin@exemple.com', '', 1, 1, 0, 'admin', '#0c1b8c'),
+(8, 'sabri', '$2y$10$ILsnMZIf9DRCxJxdQ93w1.6rmOZE/PMb3ZKbgUXcmaw6FCq6UBrey', 'younesysabr53i@gmail.com', 'uploads/télécharger.jpg', 1, 0, 1000, 'user', '#8d570c'),
+(9, 'xpt', '$2y$10$UwpZO.PuGbGnSEpQTaAFI.GYCouFnilJ5Im64a8IJ4T1IHs7obdVe', 'younesysabri@gmail.com', 'uploads/avatar.jpg', 1, NULL, 200, 'user', '#0c1b8c'),
+(10, 'Flav', '$2y$10$2BRLdWiU3ONtqF6/BqKTjOU3kQEz1QMzpgp63FCe5Wc/1xhbitt5i', 'maximilien.goffette@icloud.com', 'uploads/avatar.jpg', 1, 1, 1000, 'user', '#e6a519'),
+(11, 'Flavie', '$2y$10$Tgjk7zjCcAQFlD1oTu9y/eRtaKENXeo4ioedXaAonIXK6NNInl9qG', 'flavie.cisse1@gmail.com', 'uploads/avatar.jpg', 1, 0, 1000, 'user', '#162bd0');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
