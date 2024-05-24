c037b74f74e2a3cce0f1d480686f2fc224f8911d
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-05-24 19:14:47 +0200

    Update Ajout.php

--- Ajout.php
+++ Ajout.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ajout de dépenses</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
+  <script src="couleur.js"></script>
 </head>
 <body>
 
