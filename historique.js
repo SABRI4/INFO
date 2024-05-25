187c5d07dad8e09ced34fec28f6127d7bf1be6e3
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-05-25 12:49:06 +0200

    Update historique.js

--- historique.js
+++ historique.js
@@ -193,4 +193,4 @@ function rechercherDepenses(termeRecherche) {
     afficherResultatsRecherche(data);
   })
   .catch(error => console.error('Erreur lors de la recherche de dépenses:', error));
-}
+} 
